d1ffc55e76261925252722a41045f7cf8803bde6
OpenSourceBefikra <navneetdas53@gmail.com>
2017-01-04 17:35:44 +0530

    Shortened the file for same use

--- RmOrigFIles.sh
+++ RmOrigFIles.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-rm *.orig
-rm *.backup
-rm *.base
-rm *.local
-rm *.remote
-rm *.Orig
-rm *.Backup
-rm *.Base
-rm *.Local
-rm *.Remote
+rm *.orig *.backup *.base *.local *.remote *.Orig *.Backup *.Base *.Local *.Remote
