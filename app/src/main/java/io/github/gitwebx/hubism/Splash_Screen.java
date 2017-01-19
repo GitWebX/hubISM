1ec6983ff5d097007beb944474042a29d98bb603
abhinavbajpai2012 <abhinavbajpai2012@gmail.com>
2017-01-19 23:29:45 +0530

    Added initial files for beta version

---
+++ app/src/main/java/io/github/gitwebx/hubism/Splash_Screen.java
@@ -0,0 +1,28 @@
+package io.github.gitwebx.hubism;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash_Screen.this,MainFrame.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 1000);
+    }
+}
